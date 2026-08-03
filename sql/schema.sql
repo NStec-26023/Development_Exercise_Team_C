c450848325702aa0982f01ee3475c1fed4ebd84a
nstec26_008 <nstec26_008@fullness.email>
2026-08-04 08:18:25 +0900

    intからintegerに変更

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,7 @@
 /* departmentテーブル */
 CREATE TABLE department (
     /* 部署ID：主キー */
-    dept_id int ,
+    dept_id integer ,
     /* 部署名：100文字以内で保存 */
     name VARCHAR(100),
     primary key (dept_id)
@@ -16,13 +16,13 @@ CREATE sequence seq_deptno
 /* employeeテーブル */
 CREATE TABLE employee (
     /* 社員ID：主キー */
-    emp_id int ,
+    emp_id integer ,
     /* 社員名：100文字以内で保存 */
     name VARCHAR(100),
     /* 社員名カナ：100文字以内で保存 */
     kana VARCHAR(100),
     /* 部署ID*/
-    dept_id int,
+    dept_id integer,
     primary key (emp_id),
     FOREIGN key (dept_id) references department (dept_id)
 );
@@ -36,13 +36,13 @@ CREATE sequence seq_empno
 /* employee_accountテーブル */
 CREATE TABLE employee_account (
     /* アカウントID：主キー */
-    acc_id int ,
+    acc_id integer ,
     /* アカウント名：20文字以内で保存 */
     name VARCHAR(20),
     /* パスワード：225文字以内で保存 */
     password VARCHAR(225),
     /* 社員ID */
-    emp_id int,
+    emp_id integer,
     primary key (acc_id),
     FOREIGN key (acc_id) references employee (emp_id)
 );
@@ -56,7 +56,7 @@ CREATE sequence seq_accno
 /* product_categoryテーブル */
 CREATE TABLE product_category (
     /* 商品カテゴリID：主キー */
-    cat_id int ,
+    cat_id integer ,
     /* 商品カテゴリ名：30文字以内で保存 */
     name VARCHAR(30),
     primary key (cat_id)
@@ -71,17 +71,17 @@ CREATE sequence seq_catno
 /* productテーブル */
 CREATE TABLE product (
     /* 商品ID：主キー */
-    pro_id int ,
+    pro_id integer ,
     /* 商品名：100文字以内で保存 */
     name VARCHAR(100),
     /* 価格 */
-    price int,
+    price integer,
     /* 画像URL：200文字以内で保存 */
     image_url VARCHAR(200),
     /* 商品カテゴリID */
-    cat_id int,
+    cat_id integer,
     /* 削除フラグ */
-    delete_flg int,
+    delete_flg integer,
     primary key (pro_id),
     FOREIGN key (pro_id) references product_category (cat_id)
 );
@@ -95,11 +95,11 @@ CREATE sequence seq_prono
 /* product_stoテーブル */
 CREATE TABLE product_sto (
     /* 商品在庫ID：主キー */
-    sto_id int ,
+    sto_id integer ,
     /* 商品在庫数 */
-    quantity int,
+    quantity integer,
     /* 商品ID */
-    pro_id int,
+    pro_id integer,
     primary key (sto_id),
     FOREIGN key (pro_id) references product (pro_id)
 );
@@ -113,7 +113,7 @@ CREATE sequence seq_stono
 /* order_statusテーブル */
 CREATE TABLE order_status (
     /* 注文ステータスID：主キー */
-    sta_id int ,
+    sta_id integer ,
     /* 注文ステータス名：100文字以内で保存 */
     name VARCHAR(100),
     primary key (sta_id)
@@ -128,7 +128,7 @@ CREATE sequence seq_stano
 /* customerテーブル */
 CREATE TABLE customer (
     /* 顧客ID：主キー */
-    cus_id int ,
+    cus_id integer ,
     /* 顧客名：20文字以内で保存 */
     name VARCHAR(20),
     /* 住所1：100文字以内で保存 */
@@ -158,7 +158,7 @@ CREATE sequence seq_cusno
 /* payment_methodテーブル */
 CREATE TABLE payment_method (
     /* 支払い方法ID：主キー */
-    pay_id int ,
+    pay_id integer ,
     /* 支払い方法名：100文字以内で保存 */
     name VARCHAR(100),
     primary key (pay_id)
@@ -173,17 +173,17 @@ CREATE sequence seq_payno
 /* ordersテーブル */
 CREATE TABLE orders (
     /* 注文ID：主キー */
-    ord_id int ,
+    ord_id integer ,
     /* 注文日 */
     order_date TIMESTAMP,
     /* 合計金額 */
-    amount_total int,
+    amount_total integer,
     /* 顧客ID */
-    cus_id int,
+    cus_id integer,
     /* 注文ステータスID */
-    sta_id int,
+    sta_id integer,
     /* 支払い方法ID */
-    pay_id int,
+    pay_id integer,
     primary key (ord_id),
     FOREIGN key (cus_id) references customer (cus_id),
     FOREIGN key (sta_id) references order_status (sta_id),
@@ -198,13 +198,13 @@ CREATE sequence seq_ordno
 /* orders_detailテーブル */
 CREATE TABLE orders_detail (
     /* 注文明細ID：主キー */
-    det_id int ,
+    det_id integer ,
     /* 注文ID */
-    ord_id int,
+    ord_id integer,
     /* 商品ID */
-    pro_id int,
+    pro_id integer,
     /* 購入数 */
-    counts int,
+    counts integer,
     primary key (det_id),
     FOREIGN key (ord_id) references orders (ord_id),
     FOREIGN key (pro_id) references product (pro_id)
