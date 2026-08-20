7d224026df42e84431a38999a13e4a07b614880a
NStec-26015 <nstec26_015@fullness.email>
2026-08-20 16:49:59 +0900

    UC014のコードを書くための下準備_パッケージ作成とクラス作成

---
+++ src/main/java/com/example/fullness/stationary/entity/ProductCategory.java
@@ -0,0 +1,19 @@
+package com.example.fullness.stationary.entity;
+
+import lombok.Data;
+
+@Data
+public class ProductCategory {
+    /**
+     * 商品カテゴリID
+     * 
+     */
+    private Integer catId;
+
+    /**
+     * 商品カテゴリ名
+     */
+    private String name;
+
+
+}
---
+++ src/main/java/com/example/fullness/stationary/service/ProductCategoryRegisterService.java
@@ -0,0 +1,12 @@
+package com.example.fullness.stationary.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * UC014商品カテゴリ登録：サービス
+ */
+@Service
+public class ProductCategoryRegisterService {
+    /** 商品カテゴリ登録 */
+
+}
