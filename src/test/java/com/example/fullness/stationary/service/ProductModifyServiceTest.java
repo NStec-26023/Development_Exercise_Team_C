b9542b9b411932e4e2cd231661950eda3e713def
Uchiyama Hikaru <nstec26_023@fullness.email>
2026-08-21 14:52:35 +0900

    レビュー修正点の変更

--- src/test/java/com/example/fullness/stationary/service/ProductModifyServiceTest.java
+++ src/test/java/com/example/fullness/stationary/service/ProductModifyServiceTest.java
@@ -1,10 +1,22 @@
 package com.example.fullness.stationary.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//商品修正サービスクラスのテスト
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.fullness.stationary.entity.Product;
+import com.example.fullness.stationary.entity.ProductCategory;
+import com.example.fullness.stationary.util.TestUtil;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
 public class ProductModifyServiceTest {
 
 }
