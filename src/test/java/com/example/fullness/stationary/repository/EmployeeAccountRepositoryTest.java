c8b0cd57939aa18c2986894d0f49b47ac0a6bcca
NStec-26002 <nstec26_002@fullness.email>
2026-08-24 16:18:20 +0900

    単体テストコード作成（PWハッシュ化未実装）

---
+++ src/test/java/com/example/fullness/stationary/repository/EmployeeAccountRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.fullness.stationary.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import com.example.fullness.stationary.controller.form.EmployeeRegisterForm;
+import com.example.fullness.stationary.entity.Employee;
+import com.example.fullness.stationary.entity.EmployeeAccount;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class EmployeeAccountRepositoryTest {
+    @Autowired
+    EmployeeAccountRepository employeeAccountRepository;
+
+    @Test
+    public void testSelectUnregisteredEmployees() {
+        List<Employee> actual = employeeAccountRepository.selectUnregisteredEmployees();
+
+        List<Employee> expected = new ArrayList<>();
+
+        Employee emp1 = new Employee();
+        emp1.setEmpId(1001);
+        emp1.setName("フルネス太郎");
+        emp1.setKana("フルネスタロウ");
+        emp1.setDeptId(101);
+        expected.add(emp1);
+
+        Employee emp2 = new Employee();
+        emp2.setEmpId(1004);
+        emp2.setName("高橋優太");
+        emp2.setKana("タカハシユウタ");
+        emp2.setDeptId(101);
+        expected.add(emp2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void TestInsertAccount() {
+
+        EmployeeAccount expected = new EmployeeAccount();
+        expected.setEmpId(999);
+        expected.setName("testuser");
+        expected.setPassword("$2a$12$ozrjKbYznS/zgpJTljGk4uPCCocQuVWCQBvmY2rHL1R4KWnY5bCPC");// ハッシュ値にすべき
+
+        EmployeeRegisterForm form = new EmployeeRegisterForm();
+        form.setEmpId(999);
+        form.setAccountName("testuser");
+        form.setPassword("testuserpassword");// 生PW
+        employeeAccountRepository.insertAccount(form);
+        EmployeeAccount actual = employeeAccountRepository.findByName("testuser");
+
+        assertNotNull(actual);
+        assertEquals(expected.getEmpId(), actual.getEmpId());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getPassword(), actual.getPassword());
+
+    }
+}
---
+++ src/test/java/com/example/fullness/stationary/service/EmployeeRegisterServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.fullness.stationary.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.fullness.stationary.entity.Employee;
+
+@SpringBootTest
+public class EmployeeRegisterServiceTest {
+
+    @Autowired
+    EmployeeRegisterService employeeRegisterService;
+
+    @Test
+    public void getUnregisteredEmployeeList() {
+
+        List<Employee> actual = employeeRegisterService.getUnregisteredEmployeeList();
+
+        List<Employee> expected = new ArrayList<>();
+
+        Employee emp1 = new Employee();
+        emp1.setEmpId(1001);
+        emp1.setName("フルネス太郎");
+        emp1.setKana("フルネスタロウ");
+        emp1.setDeptId(101);
+        expected.add(emp1);
+
+        Employee emp2 = new Employee();
+        emp2.setEmpId(1004);
+        emp2.setName("高橋優太");
+        emp2.setKana("タカハシユウタ");
+        emp2.setDeptId(101);
+        expected.add(emp2);
+
+        assertEquals(expected, actual);
+    }
+}
